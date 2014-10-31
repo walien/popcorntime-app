49c7c901c94e23d34542f4b10b5acc79c311a63d
sammuel86 <blackpython.py@outlook.com>
2014-10-31 14:14:20 -0700

    begin work on popcorn streamer package

---
+++ src/content/packages/popcorn-streamer/lib/streamer.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/*
+ * We import our depedencies
+ */
+var App = require('pdk'),
+    _ = require('underscore');
+
+/*
+ * We build and export our new package
+ */
+module.exports = App.Providers.Source.extend({
+
+    start: function() {
+
+    },
+    stop: function() {
+
+    },
+    updateInfo: function() {}
+
+
+});
