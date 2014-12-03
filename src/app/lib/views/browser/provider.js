ac2350f636ee096e800d62e69c7e10c141e535f4
phnz <hello@popcorntime.io>
2014-12-03 16:42:17 -0500

    We don't need model in the browser

--- src/app/lib/views/browser/provider.js
+++ src/app/lib/views/browser/provider.js
@@ -5,29 +5,6 @@
         collectionModel: App.Model.Collection.extend({
             popid: 'imdb_id',
             type: 'movies',
-            model: Backbone.Model.extend({
-                events: {
-                    'change:torrents': 'updateHealth',
-                },
-
-                idAttribute: 'imdb_id',
-
-                initialize: function () {
-                    this.updateHealth();
-                },
-
-                updateHealth: function () {
-                    var torrents = this.get('torrents');
-
-                    _.each(torrents, function (torrent) {
-                        torrent.health = Common.healthMap[Common.calcHealth(torrent)];
-                    });
-
-                    this.set('torrents', torrents, {
-                        silent: true
-                    });
-                }
-            }),
             getProviders: function () {
                 var subtitles = false,
                     metadata = false;
