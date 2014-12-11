43f22fabed3572e7d41c4e69cef290aab48f9ce2
phnz <hello@popcorntime.io>
2014-12-11 14:32:29 -0500

    Make NSIS build async

--- build/tasks/mknsis-task.js
+++ build/tasks/mknsis-task.js
@@ -5,21 +5,28 @@ var _ = require('underscore-plus');
 module.exports = function(grunt) {
 
 	grunt.registerTask('mknsis', 'Generate Windows Installer', function() {
+		var done = this.async();
 
         var exec = require('child_process').exec;
 
         var cmd = "C:/psmodules/makensis.exe";
         var rootPath = grunt.config.get('popcorntime.rootPath');
 		if (fs.isFileSync(cmd)) {
-			return exec(cmd + ' installer.nsi', options = {
-				cwd: path.join(rootPath, 'dist', 'windows'),
+			return exec(cmd + ' ' + path.join('dist','windows','installer.nsi'), options = {
+				cwd: path.join(rootPath),
 				maxBuffer: Infinity
 			}, function(error, stdout, stderr) {
-			grunt.log.ok("Windows Installer created");
+				if (error) {
+					console.log(error);
+					process.exit(1);
+				}
+				console.log(stdout);
+				grunt.log.ok("Windows Installer created");
+				return done();
 		});
 		} else {
 			grunt.log.ok("C:/psmodules/makensis.exe not found");
+			return done();
 		}
-		return;
 	});
 }
